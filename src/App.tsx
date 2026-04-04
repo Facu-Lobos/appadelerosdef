be0a732ff6c3c9e74df1d4d7670092a227d138ba
Facundo Lobos <facundo.lobos90@gmail.com>
2026-04-04 11:08:55 -0300

    fix(routing): make /player/tournaments/:id a public route

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,6 @@ export default function App() {
                   <Route index element={<PlayerHome />} />
                   <Route path="bookings" element={<PlayerBookings />} />
                   <Route path="tournaments" element={<PlayerTournaments />} />
-                  <Route path="tournaments/:id" element={<TournamentResults />} />
                   <Route path="community" element={<PlayerCommunity />} />
                   <Route path="profile" element={<PlayerProfilePage />} />
                   <Route path="club/:id" element={<ClubDetails />} />
@@ -89,6 +88,7 @@ export default function App() {
                 {/* Publicly accessible layout routes */}
                 <Route element={<MainLayout />}>
                   <Route path="/public/torneo/:id" element={<TournamentResults />} />
+                  <Route path="/player/tournaments/:id" element={<TournamentResults />} />
                   <Route path="/public/torneo/match/:id" element={<MatchSharePage />} />
                 </Route>
 
